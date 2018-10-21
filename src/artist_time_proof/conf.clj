790970cb70b4ae715820e8bfb64c2a79c8dc9214
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-10-21 19:30:30 +0200

    Implemented listing repositories.

---
+++ src/artist_time_proof/conf.clj
@@ -0,0 +1,7 @@
+(ns artist-time-proof.conf)
+
+(def auth {:user "b*******@***********-***.com"
+           :pass "****************************************************"})
+
+(def azure-conifg {:organization "c*********"
+                   :team-project "C*********"})
--- src/artist_time_proof/core.clj
+++ src/artist_time_proof/core.clj
@@ -1,23 +1,20 @@
 (ns artist-time-proof.core
   (:require
+    [artist-time-proof.conf :refer :all]
     [clj-http.client :as client]
     [clojure.tools.cli :as cli])
   (:gen-class))
 
-(def azure-config {:organization "c*********"
-                   :team-project "C*********"
-                   :area "some-area"
-                   :resource "some-resource"
-                   :version 4.1})
+(defn- azure-url [area resource version]
+  (format "https://dev.azure.com/%s/%s/_apis/%s/%s?api-version=%s"
+    (azure-conifg :organization)
+    (azure-conifg :team-project)
+    area
+    resource
+    version))
 
-(defn- azure-dev-url []
-  (str "https://dev.azure.com/"
-    (azure-config :organization)
-    "/"
-    (azure-config :team-project)))
-
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (azure-dev-url)))
+(defn -main [& args]
+  (let [list-repositories-url (azure-url "git" "repositories" "4.1")
+        basic-auth {:basic-auth [(auth :user) (auth :pass)]}]
+    (println
+      (client/get list-repositories-url basic-auth))))
