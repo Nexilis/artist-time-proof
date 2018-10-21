1bb2dd68b06ac337ffe223abd7155de5a7794068
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-10-21 16:05:42 +0200

    Worked on preparation of Azure's API url.

--- src/artist_time_proof/core.clj
+++ src/artist_time_proof/core.clj
@@ -4,7 +4,20 @@
     [clojure.tools.cli :as cli])
   (:gen-class))
 
+(def azure-config {:organization "c*********"
+                   :team-project "C*********"
+                   :area "some-area"
+                   :resource "some-resource"
+                   :version 4.1})
+
+(defn- azure-dev-url []
+  (str "https://dev.azure.com/"
+    (azure-config :organization)
+    "/"
+    (azure-config :team-project)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (azure-dev-url)))
