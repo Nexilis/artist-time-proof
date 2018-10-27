fe4990477768c5fac766fd07c614591f7d6d9e3a
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-10-27 21:44:14 +0200

    Reduced commit results to single vector.

--- src/artist_time_proof/core.clj
+++ src/artist_time_proof/core.clj
@@ -9,10 +9,9 @@
 
 (def azure-url-base "https://dev.azure.com/%s/%s/_apis/%s%s")
 
-(def custom-formatter (f/formatter "MM/dd/yyyy"))
-
-(def from-date (f/unparse custom-formatter (t/date-time 2018 1 13)))
-(def to-date (f/unparse custom-formatter (t/date-time 2019 1 13)))
+(def from-date (f/unparse (f/formatters :date-time) (t/date-time 2018 1 13)))
+(def to-date (f/unparse (f/formatters :date-time) (t/date-time 2019 1 13)))
+(def git-author "Bartek Łukasik")
 
 (defn- azure-url [resource query-params]
   (format azure-url-base
@@ -33,10 +32,20 @@
 
 ;https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/commits?searchCriteria.author={searchCriteria.author}&searchCriteria.toDate={searchCriteria.toDate}&searchCriteria.fromDate={searchCriteria.fromDate}&api-version=4.1
 (defn- commit-urls []
-  (let [query-params (str "?searchCriteria.author=" (auth :user) "&searchCriteria.toDate=" to-date "&searchCriteria.fromDate=" from-date "&api-version=4.1")]
-    (doseq [repo-id (repo-ids)]
-      (let [url (azure-url (str "git/repositories/" repo-id "/commits") query-params)]
-        (println url)))))
+  (let [query-params
+        (str "?searchCriteria.author=" git-author
+             "&searchCriteria.toDate=" to-date
+             "&searchCriteria.fromDate=" from-date
+             "&api-version=4.1")]
+    (reduce (fn [accumulator repo-id]
+                (conj accumulator
+                      (let [url (azure-url (str "git/repositories/" repo-id "/commits") query-params)]
+                        (println url)
+                        (let [get-result (json/parse-string (:body (http-client/get url basic-auth)) true)]
+                          (println get-result)
+                          get-result))))
+            []
+            (repo-ids))))
 
 (defn -main [& _]
   (commit-urls))
