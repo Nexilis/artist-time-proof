c2543a87ef2184d070ee8b4ef630673421509b60
Bartek Łukasik <Nexilis@users.noreply.github.com>
2019-01-20 21:50:23 +0100

    Work on reimplementation in asynchronous way.

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "https://github.com/Nexilis/artist-time-proof"
   :license {:name "GNU GENERAL PUBLIC LICENSE Version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [cheshire "5.8.1"]
                  [clj-time "0.15.1"]
                  [clj-http "3.9.1"]
+                 [http-kit "2.4.0-alpha2"]
                  [proto-repl "0.3.1"]]
   :main ^:skip-aot artist-time-proof.core
   :target-path "target/%s"
--- src/artist_time_proof/core.clj
+++ src/artist_time_proof/core.clj
@@ -1,7 +1,79 @@
 (ns artist-time-proof.core
   (:require
-    [artist-time-proof.commits :refer :all])
+    [artist-time-proof.conf :refer :all]
+    [org.httpkit.client :as http]
+    [cheshire.core :as json])
   (:gen-class))
 
+;; Configurations
+
+(def azure-base-url
+  (format "https://dev.azure.com/%s/"
+    (azure-config :organization)))
+
+(def basic-auth {:basic-auth [(auth :user) (auth :pass)]})
+
+
+;; Commits
+
+(defn present-commits [])
+
+(defn get-commits-from-single-repository [])
+
+(defn get-repositories [])
+
+(defn load-commits []
+ (get-repositories)
+ ;then for all repos
+ (get-commits-from-single-repository)
+ ;when all
+ (present-commits))
+
+
+;; Pull Requests
+
+(defn present-pull-requests [])
+
+(defn get-pull-requests [user-id]
+  (let [url (str azure-base-url "_apis/git/pullrequests")
+        creator-opts (conj basic-auth {:query-params {:status    "All"
+                                                      :creatorId user-id}})
+        reviewer-opts (conj basic-auth {:query-params {:status     "All",
+                                                       :reviewerId user-id}})]
+
+    (let [futures (doall (map http/get [url url]
+                                       [creator-opts reviewer-opts]))]
+      (doseq [resp futures]
+        ;; wait for server response synchronously
+        (println (-> @resp :opts :url) " status: " (:status @resp))))))
+        ;; filter via dates and return list
+
+(defn get-user-id []
+ (let [url (str azure-base-url "_apis/connectionData")
+       promise-of-user-id (promise)]
+   (http/get url basic-auth
+             (fn [{:keys [status headers body error]}]
+               (let [decoded-body (json/parse-string body true)]
+                 (deliver promise-of-user-id (:id (:authenticatedUser decoded-body))))))
+   @promise-of-user-id))
+
+(defn load-pull-requests []
+  (let [user-id (get-user-id)]
+    (println user-id)
+    ;then
+    (get-pull-requests user-id)
+    ;when all
+    (present-pull-requests)))
+
+(defn load-all []
+  (load-pull-requests)
+  (load-commits))
+
+
+;; Start
+
 (defn -main [& _]
-  (println (commits)))
+  (println "Evaluation started...")
+  (load-all)
+  (println "...evaluation ended"))
+  ;(time (Thread/sleep 2000)))
