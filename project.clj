bc545e9e5d1988f185506c0b03019b5b417e99aa
Bartek Łukasik <Nexilis@users.noreply.github.com>
2024-03-19 16:13:32 +0100

    Update dependencies using lein-ancient.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "GNU GENERAL PUBLIC LICENSE Version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.cli "0.4.1"]
-                 [org.clojure/core.async "0.4.490"]
-                 [org.clojure/core.specs.alpha "0.2.44"]
-                 [cheshire "5.8.1"]
-                 [clj-time "0.15.1"]
-                 [clj-http "3.9.1"]
-                 [clj-pdf "2.3.1"]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [org.clojure/core.async "1.6.681"]
+                 [org.clojure/core.specs.alpha "0.3.70"]
+                 [cheshire "5.12.0"]
+                 [clj-time "0.15.2"]
+                 [clj-http "3.12.3"]
+                 [clj-pdf "2.6.8"]
                  [proto-repl "0.3.1"]
-                 [com.taoensso/timbre "4.10.0"]]
+                 [com.taoensso/timbre "6.5.0"]]
   :main ^:skip-aot artist-time-proof.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
